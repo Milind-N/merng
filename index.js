93619186d7d0f769fa7af4a23a64aef913f2ce31
Milind Nayse <milindnayse@gmail.com>
2020-12-16 23:28:31 +0100

    add db connection via mongoose and models to fetch data

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const Post = require('./models/Post')
+const { MONGODB } = require('./config');
 
 const typeDefs = gql`
+  type Post{
+    id: ID!,
+    body: String!,
+    createdAt: String!,
+    username: String!
+  }
   type Query{
-    sayHi: String!
+    getPosts:[Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => 'Hello World'
+    async getPosts(){
+      try {
+        const posts = await Post.find();
+        return posts;
+      }catch(err){
+        throw new Error(err);
+      }
+    }
   }
 }
 
@@ -18,7 +35,11 @@ const server = new ApolloServer({
   resolvers
 });
 
-server.listen({ port: 5000 })
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  })
   .then(res => {
     console.log(`Server running at ${res.url}`);
   })
