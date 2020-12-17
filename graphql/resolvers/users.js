dc1bd84b97e91a36abe7eed29667a0de59abf21a
Milind Nayse <milindnayse@gmail.com>
2020-12-18 00:00:33 +0100

    put the typedefs and resolvers in separate files, user createion, validation  and add nodemon

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server')
+
+const { SECRET_KEY } = require('../../config')
+const User = require('../../models/User');
+
+module.exports = {
+  Mutation: {
+    async register(_, { registerInput: { username, email, password, confirmPassword} }) {
+      // TODO: validate user data
+      // TODO: Make sure user doesn't already exist
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError('Username is taken', {
+          errors: {
+            username: 'This username is taken'
+          }
+        })
+      }
+      // TODO: hash a password and create a auth token
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString()
+      });
+
+      const res = await newUser.save()
+
+      const token = jwt.sign({
+        id: res.id,
+        username: res.username,
+        email: res.email
+      }, SECRET_KEY, { expiresIn: '1h' })
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token
+      }
+    }
+  }
+}
