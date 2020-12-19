b78bcaec36b925a2d432223e762c04de788d5885
Milind Nayse <milindnayse@gmail.com>
2020-12-19 23:01:35 +0100

    Implement login functionality

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,13 +2,48 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server')
 
-const { validateRegisterInput } = require('../../util/validators');
+const { validateRegisterInput, validateLoginInput } = require('../../util/validators');
 const { SECRET_KEY } = require('../../config')
 const User = require('../../models/User');
 
+const generateToken = user => {
+  return jwt.sign({
+    id: user.id,
+    username: user.username,
+    email: user.email
+  }, SECRET_KEY, { expiresIn: '1h' });
+}
 
 module.exports = {
   Mutation: {
+    async login(_, { username, password }){
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if(!valid) {
+        throw new UserInputError('Wrong credentials', { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = 'User not found';
+        throw new UserInputError('User not found', { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = 'Wrong credentials';
+        throw new UserInputError('Wrong credentials', { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token
+      }
+    },
     async register(_, { registerInput: { username, email, password, confirmPassword} }) {
       // validate user data
       const { errors, valid } = validateRegisterInput(username, email, password, confirmPassword);
@@ -36,11 +71,7 @@ module.exports = {
 
       const res = await newUser.save()
 
-      const token = jwt.sign({
-        id: res.id,
-        username: res.username,
-        email: res.email
-      }, SECRET_KEY, { expiresIn: '1h' })
+      const token = generateToken(res);
 
       return {
         ...res._doc,
