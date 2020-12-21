c4512c41500c94a470fd8879c021eb3574e3d811
Milind Nayse <milindnayse@gmail.com>
2020-12-22 00:33:18 +0100

    Add a function to delete a comment

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,5 +1,5 @@
 
-const { UserInputError } = require('apollo-server');
+const { UserInputError, AuthenticationError } = require('apollo-server');
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
 
@@ -30,6 +30,24 @@ module.exports = {
         return post
       }
       else throw new UserInputError('Post not found');
+    },
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        const commentIndex = post.comments.findIndex(c => c.id === commentId);
+
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
+        } else {
+          throw new AuthenticationError('Action not allowed')
+        }
+      } else {
+        throw new UserInputError('Post not found')
+      }
     }
   }
 }
