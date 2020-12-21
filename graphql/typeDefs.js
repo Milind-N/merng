e8d3f150882ae7461499be3e6787d2aaf47f9531
Milind Nayse <milindnayse@gmail.com>
2020-12-22 00:12:48 +0100

    Add functionality to create a comment

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -6,6 +6,13 @@ module.exports = gql`
     body: String!
     createdAt: String!
     username: String!
+    comments: [Comment]!
+  }
+  type Comment {
+    id: ID!
+    username: String!
+    body: String!
+    createdAt: String!
   }
   type User {
     id: ID!
@@ -29,5 +36,8 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
   }
 `;
